ec8d7e7d3eee2d2ba8d2b33db90ca5bbc33e7732
Ana Julia Castelo Branco Fernandes <anajuliacbfernandes@gmail.com>
2023-04-20 09:22:49 -0300

    Create megabytes.py

---
+++ megabytes.py
@@ -0,0 +1,5 @@
+print ("Informe a quantidade de espaço livre em megabytes")
+livre = int (input())
+maximo = 40000
+vlrTotal = (livre/maximo) * 100
+print ("O percentual de espaço livre do HD é de", vlrTotal)
